10bfa40b51fe3bec909835e709969fd5f29b3317
yogesh130 <yogesh@resynctech.com>
2022-07-10 08:31:59 +0800

    Connected foodlist to backend

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,43 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
-import { Stack } from "@mui/material";
-import { FoodList } from "../components/FoodInfo/FoodList";
+import { Stack, Box, Typography } from "@mui/material";
+import FoodList from "../components/FoodInfo/FoodList";
 import NavBar from "../components/Navbar/NavBar";
 import CreateFoodButton from "../components/CreateFood/CreateFoodButton";
 
 const Home = () => {
   return (
-    <Stack spacing={10}>
+    <Stack spacing={14}>
       <NavBar />
       <Grid
         container
         spacing={0}
         direction="column"
         alignItems="center"
-        justifyContent="center"
-        style={{ minHeight: "100vh" }}
+  
       >
-        <Grid Grid item xs={10} sm={12} md={12}>
-          <CreateFoodButton />
+        <Grid item xs={6} sm={12} md={12}>
+          <Box className="text-center" style={{ width: "100%" }}>
+            <Typography>
+              <span className="text-2xl text-green-600 font-bold">
+                FoodHero Packages
+              </span>
+            </Typography>
+            <CreateFoodButton />
+          </Box>
         </Grid>
-        <Grid item xs={10} sm={12} md={12}>
-          <FoodList />
+
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          style={{ minHeight: "100vh" }}
+          item
+        >
+          <Grid item xs={10} sm={12} md={12}>
+            <FoodList />
+          </Grid>
         </Grid>
       </Grid>
     </Stack>
